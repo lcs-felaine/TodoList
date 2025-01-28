9e73bab50a973561c5c8362fa2dadfd911e20fc2
Elaine Fang <efang@lcs.on.ca>
2025-01-28 12:19:42 -0500

    Organize project structures.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LandingView.swift
 //  TodoList
 //
 //  Created by Fang Elaine on 2025/1/28.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    LandingView()
 }
