4cca716d76b6752e1ef3a26a7ec533785ba7891d
Elaine Fang <efang@lcs.on.ca>
2025-01-28 12:06:43 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Fang Elaine on 2025/1/28.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
